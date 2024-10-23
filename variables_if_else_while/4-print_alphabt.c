d76266c0a19cb9256c4d3111f809b17e74e2a7d4
stan-holberton <9857@holbertonstudents.com>
2024-10-23 13:01:37 +0000

    Programme qui affiche l'alphabet en minuscule sauf 'q' et 'e'

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase except 'q' and 'e',
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
