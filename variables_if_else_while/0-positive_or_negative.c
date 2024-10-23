6305865ece57ad21e1e25f6dec7c9d791b374f81
stan-holberton <9857@holbertonstudents.com>
2024-10-23 07:32:18 +0000

    Correction des indentations et suppression des espaces dans 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,30 +3,31 @@
 #include <stdio.h>
 
 /**
- * main - Assign a random number to the variable n each time it is executed
- * and print whether the number is positive, negative, or zero.
+ * main - Entry point
+ *
+ * Description: This program assigns a random number to the variable n
+ * and prints whether the number is positive, negative, or zero.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0)); /* Initialiser le générateur de nombres aléatoires */
-    n = rand() - RAND_MAX / 2; /* Générez un nombre aléatoire */
+	srand(time(0)); /* Initialize random number generator */
+	n = rand() - RAND_MAX / 2; /* Generate a random number */
 
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n == 0)
-    {
-        printf("%d is zero\n", n);
-    }
-    else
-    {
-        printf("%d is negative\n", n);
-    }
-
-    return (0);
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0); /* Return success value */
 }
