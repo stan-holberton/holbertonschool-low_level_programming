67f4a1bacafc11c928bde4817a1a14f938e0a825
stan-holberton <9857@holbertonstudents.com>
2024-10-23 12:47:52 +0000

    Correction des espaces et utilisation de tabulations dans 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,15 @@
  */
 int main(void)
 {
-    char ch;
+	char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-        putchar(ch);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
 
-    for (ch = 'A'; ch <= 'Z'; ch++)
-        putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
