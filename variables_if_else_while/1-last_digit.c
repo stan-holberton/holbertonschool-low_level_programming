b1de657d90438fe6a98f6223d4ad0069b0a3f91a
stan-holberton <9857@holbertonstudents.com>
2024-10-23 07:23:48 +0000

    Corrige la longueur de ligne pour respecter les règles de Betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,26 +13,27 @@
  */
 int main(void)
 {
-    int n;
-    int last_digit;
+	int n;
+	int last_digit;
 
-    srand(time(0));  /* Initialize random number generator */
-    n = rand() - RAND_MAX / 2;  /* Generate a random number */
+	srand(time(0)); /* Initialize random number generator */
+	n = rand() - RAND_MAX / 2; /* Generate a random number */
 
-    last_digit = n % 10;  /* Get the last digit of n */
+	last_digit = n % 10; /* Get the last digit of n */
 
-    if (last_digit > 5)
-    {
-        printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-    }
-    else if (last_digit == 0)
-    {
-        printf("Last digit of %d is %d and is 0\n", n, last_digit);
-    }
-    else
-    {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-    }
-
-    return (0);  /* Return success value */
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+		       n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		       n, last_digit);
+	}
+	return (0); /* Return success value */
 }
