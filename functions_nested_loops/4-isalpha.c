042e67eb3152d01375fbc65ab82d21d97a0ab66a
stan-holberton <9857@holbertonstudents.com>
2024-10-25 09:11:16 +0000

    Ajout de la fonction _isalpha pour vérifier les caractères alphabétiques

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is an alphabetic character
+ * @c: the character to check
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+return (0);
+}
