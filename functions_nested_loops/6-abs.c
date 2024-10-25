06bce49c928bfda286604f2350b119922980dda0
stan-holberton <9857@holbertonstudents.com>
2024-10-25 14:09:35 +0000

    Ajoute la fonction _abs pour calculer la valeur absolue d'un entier et les tests associés

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - Calcule la valeur absolue d'un entier
+ * @n: L'entier dont on veut la valeur absolue
+ *
+ * Return: La valeur absolue de n
+ */
+int _abs(int n)
+{
+return (n < 0 ? -n : n);
+}
