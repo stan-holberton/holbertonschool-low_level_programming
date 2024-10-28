3379ecb8340fece56130082503b839a4666986cc
stan-holberton <9857@holbertonstudents.com>
2024-10-28 15:20:52 +0000

    Ajout du programme FizzBuzz avec respect du style Betty

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,25 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * fizz_buzz - prints the numbers from 1 to 100
- * followed by a new line. But for multiples of three
- * print Fizz instead of the number and for the multiples
- * of five print Buzz. For numbers which are multiples of
- * both three and five print FizzBuzz.
+ * main - prints the numbers from 1 to 100
+ *
+ * Return: Always 0 (Success)
  */
-void fizz_buzz(void)
+int main(void)
 {
 int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-_putchar('F'), _putchar('i'), _putchar('z'), _putchar('z'), _putchar('B'), _putchar('u'), _putchar('z'), _putchar('z');
+printf("FizzBuzz");
 else if (i % 3 == 0)
-_putchar('F'), _putchar('i'), _putchar('z'), _putchar('z');
+printf("Fizz");
 else if (i % 5 == 0)
-_putchar('B'), _putchar('u'), _putchar('z'), _putchar('z');
+printf("Buzz");
 else
-_putchar(i + '0');
-_putchar(' ');
+printf("%d", i);
+if (i < 100)
+printf(" ");
 }
-_putchar('\n');
+printf("\n");
+return (0);
 }
