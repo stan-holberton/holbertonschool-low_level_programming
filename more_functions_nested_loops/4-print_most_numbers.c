e6086243148a318c5f9933d65501791e09ce3e66
stan-holberton <9857@holbertonstudents.com>
2024-10-28 10:31:03 +0000

    Ajoute la documentation Betty pour print_most_numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
+/**
+ * print_most_numbers - imprime les chiffres de 0 à 9, en omettant 2 et 4
+ *
+ * Return: void
+ */
 void print_most_numbers(void)
 {
 char c;
 for (c = '0'; c <= '9'; c++)
 {
 if (c != '2' && c != '4')
+{
 _putchar(c);
+}
 }
 _putchar('\n');
 }
