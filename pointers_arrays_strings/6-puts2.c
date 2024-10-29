c2461d37bfbffc10a17d4200732bda87d8ad19f3
stan-holberton <9857@holbertonstudents.com>
2024-10-29 17:36:15 +0000

    Correction de puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,21 @@
 #include "main.h"
 /**
-*puts2 - Prints every other character of a string.
-*@str: The string to be printed.
-*
-*Description: This function prints every other character of a string,
-*              starting with the first character, followed by a new line.
-*/
+ * puts2 - Prints every other character of a string
+ * @str: The string to be processed
+ *
+ * Description: This function prints every other character
+ * of the given string, starting with the first character,
+ * followed by a new line.
+ */
 void puts2(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0'; i += 2)
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
 {
 _putchar(str[i]);
+}
 }
 _putchar('\n');
 }
