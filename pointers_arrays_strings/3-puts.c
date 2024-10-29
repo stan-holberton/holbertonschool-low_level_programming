828bdb581bd40f775569e3fe0e773eec9aed6e9c
stan-holberton <9857@holbertonstudents.com>
2024-10-29 09:15:49 +0000

    Ajoute une description pour la fonction _puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,16 @@
 #include "main.h"
-#include <unistd.h>
 
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: The string to be printed.
+ *
+ * Description: This function takes a string as a parameter and prints
+ *it character by character to the standard output, followed
+ *by a new line.
+ */
 void _puts(char *str)
 {
-while (*str)
+while(*str)
 {
 _putchar(*str);
 str++;
