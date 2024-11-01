f9dccaea43a7c5f339414f2eb64f310f2f355d05
stan-holberton <9857@holbertonstudents.com>
2024-11-01 15:01:10 +0000

    Correction de la fonction _strncpy pour respecter le style de Betty

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string up to n bytes
+ * _strncpy - copies a string
  * @dest: destination string
  * @src: source string
- * @n: number of bytes to copy from src
- *
- * Return: pointer to dest
+ * @n: number of bytes to copy
+ * Return: pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
 for (i = 0; i < n && src[i] != '\0'; i++)
+{
 dest[i] = src[i];
+}
 for (; i < n; i++)
+{
 dest[i] = '\0';
-
-return dest;
+}
+return (dest);
 }
