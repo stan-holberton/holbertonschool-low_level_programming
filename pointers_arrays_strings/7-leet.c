5ee8dcb5d21d86a228167a5c69d2f370c8d0f4e3
stan-holberton <9857@holbertonstudents.com>
2024-11-02 10:47:25 +0000

    Ajout de la fonction leet pour encoder une chaîne en 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* leet - encodes a string into 1337
+* @str: the string to encode
+* Return: the encoded string
+*/
+char *leet(char *str)
+{
+char letters[] = "aAeEoOtTlL";
+char leet[] = "4433007711";
+int i, j;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; letters[j] != '\0'; j++)
+{
+if (str[i] == letters[j])
+{
+str[i] = leet[j];
+break;
+}
+}
+}
+return (str);
+}
