b8c497528aa5a69d20e150d6ae358b7f915d6e08
stan-holberton <9857@holbertonstudents.com>
2024-11-02 10:31:21 +0000

    Ajout de la fonction string_toupper et du fichier de test sans espace en début de ligne

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*main - check the code
+* main - check the code
 *
-*Return: Always 0.
+* Return: Always 0.
 */
 int main(void)
 {
-char s[10] = "My School";
-printf("%s\n", s);
-rev_string(s);
-printf("%s\n", s);
+char str[] = "Look up!\n";
+char *ptr;
+ptr = string_toupper(str);
+printf("%s", ptr);
+printf("%s", str);
 return (0);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _atoi(char *s);
 int _strcmp(char *s1, char *s2);
+char *string_toupper(char *);
 void reverse_array(int *a, int n);
 
 #endif /* MAIN_H */
