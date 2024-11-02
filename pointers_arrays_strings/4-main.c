6b614151e83a59362dde6edb48a73a8202481188
stan-holberton <9857@holbertonstudents.com>
2024-11-02 10:24:54 +0000

    Ajout de la fonction reverse_array et du fichier de test sans espace en début de ligne

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,13 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 /**
-*main - check the code
+* print_array - prints an array of integers
+* @a: an array of integers
+* @n: the number of elements to print
+*/
+void print_array(int *a, int n)
+{
+int i;
+i = 0;
+while (i < n)
+{
+if (i != 0)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i++;
+}
+printf("\n");
+}
+/**
+* main - check the code
 *
-*Return: Always 0.
+* Return: Always 0.
 */
 int main(void)
 {
-char *str;
-str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-print_rev(str);
+int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+print_array(a, sizeof(a) / sizeof(int));
+reverse_array(a, sizeof(a) / sizeof(int));
+print_array(a, sizeof(a) / sizeof(int));
 return (0);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _atoi(char *s);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 
 #endif /* MAIN_H */
