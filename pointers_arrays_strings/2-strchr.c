57316744f3a5379fc2f8b86b0ce45ab41a26be6d
stan-holberton <9857@holbertonstudents.com>
2024-11-04 10:25:21 +0000

    Ajout de la fonction _strchr pour localiser un caractère dans une chaîne. - Implémentation conforme au style Betty - Inclusion de stddef.h pour la définition de NULL - Tests effectués avec succès

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * _strchr - locates a character in a string
- * @s: string to search
- * @c: character to locate
+ * _strchr - locate a character in a string.
+ * @s: the string to search in.
+ * @c: the character to locate.
  *
- * Return: pointer to the first occurrence of c or NULL
+ * Return: a pointer to the first occurrence of c in s, or NULL if not found.
  */
 char *_strchr(char *s, char c)
 {
@@ -13,13 +14,9 @@ while (*s)
 {
 if (*s == c)
 {
-return (s);
+return s;
 }
 s++;
 }
-if (*s == c)
-{
-return (s);
-}
-return (NULL);
+return NULL;
 }
