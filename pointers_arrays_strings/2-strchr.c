7d5e7710ac8b40c24dbc0c2cca9fff3dc076dc98
stan-holberton <9857@holbertonstudents.com>
2024-11-04 10:16:20 +0000

    Implémentation de la fonction _strchr avec 2-strchr.c et modif de 2-main.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (*s == c)
+{
+return (s);
+}
+return (NULL);
+}
