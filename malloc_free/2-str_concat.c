6ce6548f90601f31e14900887b2c31ce7d33391a
stan-holberton <9857@holbertonstudents.com>
2024-11-12 13:29:55 +0000

    Ajout de la fonction str_concat avec fichier main.h inclus

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* str_concat - Concatène deux chaînes de caractères.
+* @s1: Première chaîne.
+* @s2: Deuxième chaîne.
+*
+* Return: Pointeur vers la nouvelle chaîne concaténée ou NULL si échec.
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int i, j, len1 = 0, len2 = 0;
+
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
+
+	while (s1[len1])
+	len1++;
+	while (s2[len2])
+	len2++;
+
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+	return (NULL);
+
+	for (i = 0; i < len1; i++)
+	concat[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+	concat[i + j] = s2[j];
+
+	concat[i + j] = '\0';
+
+	return (concat);
+}
