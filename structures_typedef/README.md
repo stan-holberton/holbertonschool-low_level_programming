df9833f8df307be76c078ee5a9a7cef42dc48d79
stan-holberton <9857@holbertonstudents.com>
2024-11-13 12:49:51 +0000

    Ajout de la structure dog avec les champs name, age, et owner dans dog.h et 0-main.c

---
+++ structures_typedef/README.md
@@ -0,0 +1,3 @@
+#Projet structures et typedef
+
+##Ce repertoire me permet d'apprendre les usages des structures et de typedef dans le cadre de la Holberton School. A bientot, je vous aime, biz.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure représentant un chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
