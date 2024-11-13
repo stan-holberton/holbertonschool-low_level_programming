a02b6418abd4e8d832c1238aeb88d78c67f5585a
stan-holberton <9857@holbertonstudents.com>
2024-11-13 09:33:41 +0000

    Fix: Correction du style Betty et ajout de la fonction _calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array and initializes it to zero.
+ * @nmemb: The number of elements in the array.
+ * @size: The size of each element.
+ *
+ * Return: Pointer to the allocated memory, or NULL if malloc fails or
+ *         nmemb or size is 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		((char *)ptr)[i] = 0;
+
+	return (ptr);
+}
