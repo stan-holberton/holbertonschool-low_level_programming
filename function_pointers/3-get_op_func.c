2fc30e49ac36e5227ff179fdcc8c777164ef3684
stan-holberton <9857@holbertonstudents.com>
2024-11-22 15:16:58 +0000

    correction du fichier 3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,4 +1,7 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  /* Pour utiliser strlen */
 
 /**
  * get_op_func - Sélectionne la fonction appropriée pour
@@ -10,6 +13,12 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	/* Vérification que l'opérateur est un seul caractère */
+	if (strlen(s) != 1)
+	{
+		return (NULL);  /* Si l'opérateur est plus d'un caractère, retourne NULL */
+	}
+
 	/* Tableau d'opérateurs et de fonctions correspondantes */
 	op_t ops[] = {
 		{"+", op_add},  /* Opérateur addition */
@@ -21,7 +30,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	/* Parcours tableau pour trouver  fonction correspondant à l'opérateur */
+	/* Parcours tableau pour trouver fonction correspondant à l'opérateur */
 	while (ops[i].op != NULL)
 	{
 		/* Si l'opérateur correspond à celui fourni par l'utilisateur */
@@ -32,6 +41,8 @@ int (*get_op_func(char *s))(int, int)
 		}
 		i++; /* Passe à l'opérateur suivant dans le tableau */
 	}
+
 	/* Si aucun opérateur n'a été trouvé, retourne NULL */
 	return (NULL);
 }
+
