08da62f9128f7972308526201b248a86752bd871
stan-holberton <9857@holbertonstudents.com>
2024-12-08 19:37:44 +0000

    Rechangement du fichier 3-cp.c a celui d'origne

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,77 +1,75 @@
+#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <string.h>
+
+#define BUF_SIZE 1024
 
 /**
- * close_file - close a file and check if success
- *@file_descriptor: the file descriptor
+ * error_exit - Handles errors and exits the program.
+ * @message: The error message to display.
+ * @code: The exit code.
  */
-void close_file(int file_descriptor)
+void error_exit(char *message, int code)
 {
-	if (close(file_descriptor) == -1)
+	dprintf(STDERR_FILENO, "%s", message);
+	exit(code);
+}
+
+/**
+ * copy_file - Copies the content of one file to another.
+ * @from_fd: File descriptor of the source file.
+ * @to_fd: File descriptor of the destination file.
+ *
+ * Return: 0 on success, or exits with a code on failure.
+ */
+void copy_file(int from_fd, int to_fd)
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUF_SIZE];
+
+	while ((bytes_read = read(from_fd, buffer, BUF_SIZE)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_descriptor);
-		exit(100);
+		bytes_written = write(to_fd, buffer, bytes_read);
+		if (bytes_written == -1)
+			error_exit("Error: Can't write to ", 99);
 	}
+
+	if (bytes_read == -1)
+		error_exit("Error: Can't read from file ", 98);
 }
+
 /**
- * error_99 - function for message if error 99
- *@filename: path of file
- */
-void error_99(char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	exit(99);
-}
-/**
- * error_98 - function for message if error 98
- *@filename: path of file
- */
-void error_98(char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-	exit(98);
-}
-/**
- * main - Entry point
- *@argc: number of argument
- *@argv: array of argument
- *Return: return 0 if success
+ * main - Copies the content of one file to another.
+ * @argc: The number of arguments.
+ * @argv: The arguments array.
+ *
+ * Return: 0 on success, or exits with a code on failure.
  */
 int main(int argc, char *argv[])
 {
-	char *file_from = NULL, *file_to = NULL;
-	int file_from_descriptor = 0, file_to_descriptor = 0;
-	char buffer_copy[1024];
-	ssize_t bytes_read = 0, bytes_write = 0;
+	int from_fd, to_fd;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		error_exit("Usage: cp file_from file_to\n", 97);
 
-	file_from = argv[1];
-	file_to = argv[2];
+	from_fd = open(argv[1], O_RDONLY);
+	if (from_fd == -1)
+		error_exit("Error: Can't read from file ", 98);
 
-	file_from_descriptor = open(file_from, O_RDONLY);
-	if (file_from_descriptor == -1)
-		error_98(file_from);
+	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (to_fd == -1)
+		error_exit("Error: Can't write to ", 99);
 
-	file_to_descriptor = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to_descriptor == -1)
-		error_99(file_to);
+	copy_file(from_fd, to_fd);
 
-	bytes_read = read(file_from_descriptor, buffer_copy, sizeof(buffer_copy));
-	if (bytes_read == -1)
-		error_98(file_from);
+	if (close(from_fd) == -1)
+		error_exit("Error: Can't close fd ", 100);
 
-	bytes_write = write(file_to_descriptor, buffer_copy, bytes_read);
-	if (bytes_write != bytes_read)
-		error_99(file_to);
+	if (close(to_fd) == -1)
+		error_exit("Error: Can't close fd ", 100);
 
-	close_file(file_from_descriptor);
-	close_file(file_to_descriptor);
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,11 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <sys/types.h>
+
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
 int create_file(const char *filename, char *text_content);
 
+int _putchar(char c);
+
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+void error_exit(char *msg, int code);
+
+#endif /* MAIN_H */
