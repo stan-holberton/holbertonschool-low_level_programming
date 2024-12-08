6b3bc3c622411a7776f6559f0110e8f1f03d883a
stan-holberton <9857@holbertonstudents.com>
2024-12-08 18:33:26 +0000

    fixe bug

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,17 +6,39 @@
 #include <string.h>
 
 #define BUF_SIZE 1024
-#define BUF_SIZE 1024
 
 /**
  * error_exit - Handles errors and exits the program.
  * @message: The error message to display.
  * @code: The exit code.
  */
-void error_exit(const char *message, int code)
+void error_exit(char *message, int code)
 {
 	dprintf(STDERR_FILENO, "%s", message);
 	exit(code);
+}
+
+/**
+ * copy_file - Copies the content of one file to another.
+ * @from_fd: File descriptor of the source file.
+ * @to_fd: File descriptor of the destination file.
+ *
+ * Return: 0 on success, or exits with a code on failure.
+ */
+void copy_file(int from_fd, int to_fd)
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUF_SIZE];
+
+	while ((bytes_read = read(from_fd, buffer, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(to_fd, buffer, bytes_read);
+		if (bytes_written == -1)
+			error_exit("Error: Can't write to ", 99);
+	}
+
+	if (bytes_read == -1)
+		error_exit("Error: Can't read from file ", 98);
 }
 
 /**
@@ -28,34 +50,25 @@ void error_exit(const char *message, int code)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, read_bytes, write_bytes;
-	char buffer[BUF_SIZE];
+	int from_fd, to_fd;
 
 	if (argc != 3)
 		error_exit("Usage: cp file_from file_to\n", 97);
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	from_fd = open(argv[1], O_RDONLY);
+	if (from_fd == -1)
 		error_exit("Error: Can't read from file ", 98);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		error_exit("Error: Can't write to file ", 99);
+	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (to_fd == -1)
+		error_exit("Error: Can't write to ", 99);
 
-	while ((read_bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
-	{
-		write_bytes = write(fd_to, buffer, read_bytes);
-		if (write_bytes == -1)
-			error_exit("Error: Can't write to file ", 99);
-	}
+	copy_file(from_fd, to_fd);
 
-	if (read_bytes == -1)
-		error_exit("Error: Can't read from file ", 98);
-
-	if (close(fd_from) == -1)
+	if (close(from_fd) == -1)
 		error_exit("Error: Can't close fd ", 100);
 
-	if (close(fd_to) == -1)
+	if (close(to_fd) == -1)
 		error_exit("Error: Can't close fd ", 100);
 
 	return (0);
--- file_io/main.h
+++ file_io/main.h
@@ -9,7 +9,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _putchar(char c);
 int append_text_to_file(const char *filename, char *text_content);
-void error_exit(const char *msg, int code);
-
+void error_exit(char *msg, int code);
 
 #endif /* MAIN_H */
