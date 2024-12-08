b5a8aa4d2fc674ad473fc0d01cd7041457e81df6
stan-holberton <9857@holbertonstudents.com>
2024-12-08 19:30:06 +0000

    changement du fichier 3-cp.c

--- file_io/main.h
+++ file_io/main.h
@@ -1,14 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stddef.h>
-#include <unistd.h>
-#include <string.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int _putchar(char c);
-int append_text_to_file(const char *filename, char *text_content);
-void error_exit(char *msg, int code);
 
-#endif /* MAIN_H */
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
